268dea60e61bbdc19b9bd2904d134ec0da5b7e14
alex-soldatkin <alex.soldatkin@me.com>
2024-02-27 10:13:12 +0000

    sample Google API request

--- translate_api/translate.py
+++ translate_api/translate.py
@@ -1,6 +1,26 @@
 # this is a test commit for the translate.py file
 
-import requests
-import json
-import os
+from google.cloud import automl
 
+# TODO(developer): Uncomment and set the following variables
+# project_id = "YOUR_PROJECT_ID"
+# model_id = "YOUR_MODEL_ID"
+# file_path = "path_to_local_file.txt"
+
+prediction_client = automl.PredictionServiceClient()
+
+# Get the full path of the model.
+model_full_id = automl.AutoMlClient.model_path(project_id, "us-central1", model_id)
+
+# Read the file content for translation.
+with open(file_path, "rb") as content_file:
+    content = content_file.read()
+content.decode("utf-8")
+
+text_snippet = automl.TextSnippet(content=content)
+payload = automl.ExamplePayload(text_snippet=text_snippet)
+
+response = prediction_client.predict(name=model_full_id, payload=payload)
+translated_content = response.payload[0].translation.translated_content
+
+print(f"Translated content: {translated_content.content}")
